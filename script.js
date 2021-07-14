0db848400a7c6e9b064399f405e39e9fdea36a67
Álvaro J <86482525+alvaro-j@users.noreply.github.com>
2021-07-14 16:52:40 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,21 @@
 var checkbox = document.getElementById('checkbox')
-checkbox.addEventListener("change" , check)
-var price = document.getElementsByClassName('price')
+var checkbox_value = checkbox.value;
+checkbox.addEventListener("change" , check);
+var price = document.getElementsByClassName('price');
+var i;
 
 function check() {
-  var i; 
-  for (i = 0; i < price.length; i++) {
-    price[0].innerHTML = "199.99";
-    price[1].innerHTML = "249.99";
-    price[2].innerHTML = "399.99";
- }
+  if (checkbox.checked == true) {
+    for (i = 0; i < price.length; i++) {
+        price[0].innerHTML = "19.99";
+        price[1].innerHTML = "24.99";
+        price[2].innerHTML = "39.99";
+    } 
+} else {
+    for (i = 0; i < price.length; i++) {
+        price[0].innerHTML = "199.99";
+        price[1].innerHTML = "249.99";
+        price[2].innerHTML = "399.99";
+    }
+}
 }
